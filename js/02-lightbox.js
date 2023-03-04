63fae22a78a68d015a85ccee3d38f5272dccc5a3
Andrii <mecushka@gmail.com>
2023-03-04 21:35:54 +0200

    secondOne

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const createElem = createCards(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', createElem);
+
+function createCards() {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+    }).join("")
+}
+
+galleryContainer.innerHTML = createElem;
+
+galleryContainer.addEventListener('click', clickOnLink);
+
+function clickOnLink(event) {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', `
+    <img width = "1400" height = "900" src = "${event.target.dataset.source}">`)
+}
